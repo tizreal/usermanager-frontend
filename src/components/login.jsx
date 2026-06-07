9a2438589b7b398bbf412f574d43000be863bcec
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2026-06-07 14:47:30 -0400

    fixed login error

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -55,9 +55,7 @@ const Login = () => {
     initialValues: { username: "", password: "" },
     validationSchema: Yup.object({
       username: Yup.string().required("Username is required"),
-      password: Yup.string()
-        .min(6, "Password must be at least 6 characters")
-        .required("Password is required"),
+      password: Yup.string().required("Password is required"),
     }),
     onSubmit: async (values) => {
       try {
