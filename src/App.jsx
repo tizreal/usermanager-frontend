2b266343f083b5fbdada6a41e1995ab498126d23
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2026-06-07 00:30:54 -0400

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,22 @@ import Profile from "./pages/profile";
 import EditProfile from "./pages/editProfile";
 import AdminPanel from "./pages/adminPanel";
 import Home from "./pages/home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar /> {/* Renders on every page, hides itself if not logged in */}
       <div className="max-w-4xl mx-auto p-4">
         <Routes>
+          {/*Public routes*/}
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/user/verifyEmail" element={<VerifyEmail />} />
+
+          {/*Protected routes*/}
+
           <Route
             path="/home"
             element={
